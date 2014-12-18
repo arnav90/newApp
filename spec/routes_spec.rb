94f8604b1219f15793aceca7e0cbebc8c6f0b6e9
Arnav Kumar <arnavku[at]thoughtworks[dot]com>
2014-12-18 15:34:10 +0530

    Fixing Routing specs

--- spec/routes_spec.rb
+++ spec/routes_spec.rb
@@ -5,8 +5,8 @@ describe 'routing', type: :routing do
     it 'should route to #create' do
       expect(post('/drives')).to route_to('drives#create')
     end
-    xit 'should route to #show' do
-      expect(get('/drives/1')).to route_to('drives#show',id: 1)
+    it 'should route to #show' do
+      expect(get('/drives/1')).to route_to('drives#show',id: '1')
     end
     it 'should route to #new' do
       expect(get('/drives/new')).to route_to('drives#new')
