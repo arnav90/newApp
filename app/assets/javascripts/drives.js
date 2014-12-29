fa736011657d94c226453d798d3f5588ed358715
Arnav Kumar <arnav90@gmail.com>
2014-12-29 15:14:52 +0530

    Added Basic from for uploading file. WIP

--- app/assets/javascripts/drives.js
+++ app/assets/javascripts/drives.js
@@ -5,4 +5,24 @@ function newDriveModal(){
             document.getElementById('new-drive-form').submit();
         }
     }).modal('show');
-};
+}
+
+function addStudents(){
+    $('#add-students-modal').modal({
+        closable: true,
+        onApprove: function(){
+            document.getElementById('upload-students-list').submit();
+        }
+    }).modal('show');
+}
+
+function formModalTrigger(modal,form){
+    var modal = document.getElementById(modal);
+    var form = document.getElementById(form);
+    $(modal).modal({
+        closable: true,
+        onApprove: function(){
+            form.submit();
+        }
+    }).modal('show')
+}
