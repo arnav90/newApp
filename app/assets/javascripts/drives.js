80b09d37d8220d6d01efedd3639cae32ac39bf69
Arnav Kumar <arnav90@gmail.com>
2015-01-01 19:53:49 +0530

    Minor clean up

--- app/assets/javascripts/drives.js
+++ app/assets/javascripts/drives.js
@@ -5,10 +5,20 @@ function formModalTrigger(modal,form){
         closable: true,
         onApprove: function(){
             form.submit();
+        },
+        onDeny: function(){
+            form.reset();
+            fileAdded();
         }
     }).modal('show')
 }
 
 function fileUpload(){
     $('#added-file').click();
+}
+
+function fileAdded(){
+    var file=document.getElementById('added-file').value;
+    var label=document.getElementById('file-name');
+    file.length > 0 ? label.textContent = file : label.textContent = 'No File Selected';
 }
